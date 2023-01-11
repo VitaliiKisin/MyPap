03598e748b6594de0635771a0493eb5c02d466c8
VitaliiKisin <VKisin@bk.ru>
2023-01-12 01:39:50 +0400

    add Task4

---
+++ Homeworke/Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿int n;
+int i;
+int m;
+
+    n = 10;
+    i = 0;
+    m = i;
+
+     if (m <=n)
+    {
+        Console.Write(m);
+    }    
+      else 
+    {
+        Console.WriteLine("Stop");
+    }
+    m = m + 2;
+
