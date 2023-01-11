6083122ac7630eecfd821149f6c2536c12d3f2a6
VitaliiKisin <VKisin@bk.ru>
2023-01-11 19:16:08 +0400

    add Task2

---
+++ Homeworke/Task2/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("2, 3, 7");
+Console.WriteLine("max = 7");
+Console.WriteLine("44, 5, 78");
+Console.WriteLine("max = 78");
+Console.WriteLine("22, 3, 9");
+Console.WriteLine("max = 22");
