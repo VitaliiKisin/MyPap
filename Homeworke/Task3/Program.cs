1fc397e15ab039b4546a5795a5863b45767914c2
VitaliiKisin <VKisin@bk.ru>
2023-01-11 19:37:01 +0400

    add Task3

---
+++ Homeworke/Task3/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("4");
+Console.WriteLine("Делится на два, без остатка");
+Console.WriteLine("-3");
+Console.WriteLine("Делится на два c остатком");
+Console.WriteLine("7");
+Console.WriteLine("Делится на два c остатком");
