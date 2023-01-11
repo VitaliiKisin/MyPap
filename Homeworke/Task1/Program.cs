5f902bc0f2c8f8cbcb7fff830fe22b377212f3ba
VitaliiKisin <VKisin@bk.ru>
2023-01-11 18:42:34 +0400

    add Task1

---
+++ Homeworke/Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("5 и 7");
+Console.WriteLine("max = 7");
+Console.WriteLine("min = 5");
+Console.WriteLine("2 и 10");
+Console.WriteLine("max = 10");
+Console.WriteLine("min = 2" );
+Console.WriteLine("-9 и -3");
+Console.WriteLine("max = -3");
+Console.WriteLine("min = -9");
