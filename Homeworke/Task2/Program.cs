3ef98c843654397c871e48000bdd34dd502972d7
VitaliiKisin <VKisin@bk.ru>
2023-01-11 23:21:32 +0400

    add Task2

--- Homeworke/Task2/Program.cs
+++ Homeworke/Task2/Program.cs
@@ -1,6 +1,25 @@
-﻿Console.WriteLine("2, 3, 7");
-Console.WriteLine("max = 7");
-Console.WriteLine("44, 5, 78");
-Console.WriteLine("max = 78");
-Console.WriteLine("22, 3, 9");
-Console.WriteLine("max = 22");
+﻿int a;
+int b;
+int c;
+
+a = 100;
+b = 50;
+c = 25;
+
+if (a > b)
+{
+    b = a;
+}
+else
+{
+    b = b;
+}    
+if (b > c)
+{
+    Console.WriteLine(b);
+}
+else
+{
+    Console.WriteLine(c);
+}
+
