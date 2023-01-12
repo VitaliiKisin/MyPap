2b564b89c9866bbbe4124877dc21db51f6d51609
VitaliiKisin <VKisin@bk.ru>
2023-01-12 18:21:56 +0400

    add Example9

---
+++ lecture2/Example9/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = {11, 56,67, 87, 45, 67, 78, 90, 34};
+
+int n = array.Length;
+int find = 67;
+
+int index = 0;
+
+while (index < n)
+{
+
+    if(array[index] == find)
+    {
+    Console.WriteLine(index);
+    break;
+    }
+//index = index + 1;
+index++;
+}
