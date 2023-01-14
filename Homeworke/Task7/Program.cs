81906f0789c904d35e219c5c7814bc6d97afda93
VitaliiKisin <VKisin@bk.ru>
2023-01-14 23:53:14 +0400

    add Task7

---
+++ Homeworke/Task7/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.WriteLine("Введи цифру, обозначающую день недели: ");
+
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void DayOfTheWeek(int dayNumber);
+
+if (dayNumber == 6 || dayNumber == 7)
+{
+    Console.WriteLine("Выходной ");
+}
+else if (dayNumber < 1 || dayNumber > 7) 
+{
+    Console.WriteLine("Введите цифру от 1 до 7 ");
+}
+  else Console.WriteLine("Не выходной ");
+{
+DayOfTheWeek(dayNumber);
+}
