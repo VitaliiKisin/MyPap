a82e7efe54ed6e3e3b51c4d60bdb0e9c7107c24a
VitaliiKisin <VKisin@bk.ru>
2023-01-15 18:09:43 +0400

    add Task8

--- Homeworke/Task7/Program.cs
+++ Homeworke/Task7/Program.cs
@@ -1,19 +1,22 @@
 ﻿Console.Clear();
-Console.WriteLine("Введи цифру, обозначающую день недели: ");
+Console.WriteLine("Введи цифру, обозначающую день недели, от 1 до 7 ");
 
 int dayNumber = Convert.ToInt32(Console.ReadLine());
 
 void DayOfTheWeek(int dayNumber);
-
+{
 if (dayNumber == 6 || dayNumber == 7)
 {
     Console.WriteLine("Выходной ");
 }
-else if (dayNumber < 1 || dayNumber > 7) 
+else 
+if (dayNumber < 1 || dayNumber > 7) 
 {
     Console.WriteLine("Введите цифру от 1 до 7 ");
 }
-  else Console.WriteLine("Не выходной ");
+ else Console.WriteLine("Будний день ");
+}
 {
 DayOfTheWeek(dayNumber);
 }
+
---
+++ Homeworke/Task8/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число: ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number)
+{
+  if (number[0]==number[4] || number[1]==number[3])
+  {
+    Console.WriteLine($"Ваше число - Палиндром.");
+  }
+  else Console.WriteLine($"Ваше число - НЕ палиндром.");
+}
+
+if (number!.Length == 5)
+{
+  CheckingNumber(number);
+}
+else Console.WriteLine($"Введите правильное число");
+
