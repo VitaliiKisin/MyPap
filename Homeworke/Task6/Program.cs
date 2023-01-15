91a9b466077b2c0664e991dfe47a22dbe5ab8928
VitaliiKisin <VKisin@bk.ru>
2023-01-15 18:14:58 +0400

    add Task6

---
+++ Homeworke/Task6/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("Введи трёхзначное число: ");
+
+int IndexNumber = Convert.ToInt32(Console.ReadLine());
+string Number = Convert.ToString(IndexNumber);
+
+if (IndexNumber > 2)
+{
+    Console.WriteLine(Number[2]);
+}
+
+else 
+
+{
+    Console.WriteLine("Третьей цифры нет ");
+}
