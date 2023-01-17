21c6f9ac3b6fe2a195f31d4e470dad62d46a03b8
VitaliiKisin <VKisin@bk.ru>
2023-01-17 19:30:16 +0400

    add Метод For

--- lecture3/Example12/Program.cs
+++ lecture3/Example12/Program.cs
@@ -12,4 +12,14 @@ string Method4(int count, string text)
 
 
 string res = Method4(10, "z");
-Console.WriteLine(res);
+//Console.WriteLine(res);
+
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int g = 2; g <= 10; g++)
+    {
+        Console.WriteLine($"{i} * {g} = {i*g}");
+    }
+    Console.WriteLine();
+}
