a5f4b1668a124820dfcb02c49a84e29d8791840f
VitaliiKisin <VKisin@bk.ru>
2023-01-18 12:15:51 +0400

    add Task91

--- Homeworke/Task91/Program.cs
+++ Homeworke/Task91/Program.cs
@@ -11,15 +11,34 @@
 // Распечатать в новый массив
 // string <- int[]
 
-
-
 Console.Clear();
 
+Console.WriteLine("Введите N-е количество целых чисел массива через пробел ");
+//int[] arr = new int[N];
+//Console.WriteLine(String.Join(' ', arr));
+
+int arr = Convert.ToInt32(Console.ReadLine());
+string str = Convert.ToString(arr);
+
+//char [] ar = str.ToCharArray();
+
+//char [] ar = str.ToArray<char>();
+//int[] Firstarray = new int[100];
+            //int i=0;
+
+//for int[]Firstarray <= N
+//int[] Firstarray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+//string[] str = Console.ReadLine().Split();
 
 
-for(i <= N/2)
 
-number = a[i] * a[N - 1 - i]
 
-res[N/2]
-res[i] = a[i] * a[N - 1 - i];
+
+
+
+//for(i <= N/2)
+
+//number = a[i] * a[N - 1 - i]
+
+//res[N/2]
+//res[i] = a[i] * a[N - 1 - i];
