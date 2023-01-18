e5a92416f40997d279ebbe6a27d9e3cba5cd16cc
VitaliiKisin <VKisin@bk.ru>
2023-01-18 13:29:55 +0400

    add Task93

---
+++ Homeworke/Task93/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.WriteLine("Введите k1: ");
+var k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b1: ");
+var b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k2: ");
+var k2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b2: ");
+var b2 = Convert.ToDouble(Console.ReadLine());
+ 
+ 
+var x = -(b1 - b2) / (k1 - k2);
+var y = k1 * x + b1;
+ 
+x = Math.Round(x, 3);
+y = Math.Round(y, 3);
+ 
+Console.WriteLine($"Пересечение в точке: ({x};{y})");
+
+//Math.Round
